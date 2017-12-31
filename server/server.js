473f71d6572a92194cae23dce26a224eebac144d
beardedguru <charles@thebeardedguru.com>
2017-12-31 03:12:18 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // library imports
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 // local imports
 var {mongoose} = require('./db/mongoose');
@@ -36,6 +37,39 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// GET /todos/1234324
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({
+      todo
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+  // Todo.findById(id).then((todo) => {
+  //   if (!todo) {
+  //     res.status(404).send();
+  //   } else {
+  //     res.send({
+  //       todo
+  //     });
+  //   }
+  // }, (e) => {
+  //   res.status(400).send();
+  // })
+
 });
 
 module.exports = {
