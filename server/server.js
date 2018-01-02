b8ffa6bdf9f53a631995f92bcde03228c34b1587
beardedguru <charles@thebeardedguru.com>
2018-01-01 20:30:06 -0500

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 // set up for heroku
 const port = process.env.PORT;
@@ -172,6 +173,26 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
+});
+
+var authenticate = (req, res, next) => {
+  var token = req.header('x-auth');
+
+  User.findByToken(token).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    req.user = user;
+    req.token = token;
+    next();
+  }).catch((e) => {
+    res.status(401).send();
+  });
+}
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 
